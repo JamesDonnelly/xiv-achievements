8d2372d4306839095d572d67864d658de3ea389d
James M Donnelly <hello@jamesmdonnelly.com>
2017-02-14 15:11:00 +0000

    Typo: Zodiack -> Zodiark

--- src/js/components/Layout/Footer.js
+++ src/js/components/Layout/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
       <footer id="footer">
         <div class="container">
           <p>
-            &copy; James Donnelly (<i class="fa fa-twitter"></i> <a href="https://twitter.com/inb4" target="_blank">@inb4</a> on Twitter; <a href="http://xivdb.com/character/10012596/tequila+mockingbird/zodiark">Tequila Mockingbird</a> on Zodiack)<br/>
+            &copy; James Donnelly (<i class="fa fa-twitter"></i> <a href="https://twitter.com/inb4" target="_blank">@inb4</a> on Twitter; <a href="http://xivdb.com/character/10012596/tequila+mockingbird/zodiark">Tequila Mockingbird</a> on Zodiark)<br/>
             Check out the <i class="fa fa-github"></i> <a href="#">GitHub Repository</a> where bugs and suggestions can be submitted
           </p>
           <p>
