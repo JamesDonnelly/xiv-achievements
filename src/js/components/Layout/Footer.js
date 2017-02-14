f74f9f554b1bebbf74c3be9af5df97a1a16e22a2
James M Donnelly <hello@jamesmdonnelly.com>
2017-02-14 15:11:32 +0000

    Added GitHub repo link.

--- src/js/components/Layout/Footer.js
+++ src/js/components/Layout/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends React.Component {
         <div class="container">
           <p>
             &copy; James Donnelly (<i class="fa fa-twitter"></i> <a href="https://twitter.com/inb4" target="_blank">@inb4</a> on Twitter; <a href="http://xivdb.com/character/10012596/tequila+mockingbird/zodiark">Tequila Mockingbird</a> on Zodiark)<br/>
-            Check out the <i class="fa fa-github"></i> <a href="#">GitHub Repository</a> where bugs and suggestions can be submitted
+            Check out the <i class="fa fa-github"></i> <a href="https://github.com/JamesDonnelly/xiv-achievements">GitHub Repository</a> where bugs and suggestions can be submitted
           </p>
           <p>
             If you're a student, check out another site I run: <i class="fa fa-gift"></i> <a href="http://freestudentsoftware.com">FreeStudentSoftware.com</a>
